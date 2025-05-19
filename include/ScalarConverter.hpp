5ea35058e3b92efe647b3a96d5ad1efb7655fb73
ropkyns <ropkyns@gmail.com>
2025-05-19 16:31:40 +0200

    starting ex00

---
+++ include/ScalarConverter.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 16:15:10 by paulmart          #+#    #+#             */
+/*   Updated: 2025/05/19 16:25:42 by paulmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCLARCONVERTER_HPP
+
+#include <iostream>
+#include <exception>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <stdexcept>
+#include <iomanip>
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &S);
+		~ScalarConverter();
+		ScalarConverter &operator= (const ScalarConverter &S);
+	public:
+		static void convert(std::string const &arg);
+};
+
+
+#endif
